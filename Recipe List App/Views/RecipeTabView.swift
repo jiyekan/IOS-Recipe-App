354f8b02d6ed7449791788c28868d3c21d63d029
Yekan Ji <jiyekan.neu@gmail.com>
2021-06-24 14:03:33 -0700

    Add TabView as home page

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,39 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Yekan Ji on 6/24/21.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            
+            Text("Feature View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
