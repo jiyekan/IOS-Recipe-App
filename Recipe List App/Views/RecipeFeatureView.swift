c465c0fad650edac112065c39975fdac89a394d4
Yekan Ji <jiyekan.neu@gmail.com>
2021-06-25 13:37:05 -0700

    - Build Feature View

--- Recipe List App/Views/RecipeFeatureView.swift
+++ Recipe List App/Views/RecipeFeatureView.swift
@@ -12,7 +12,63 @@ struct RecipeFeatureView: View {
     @EnvironmentObject var model:RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack (alignment: .leading, spacing: 0) {
+            
+            Text("Feature Recipes")
+                .bold()
+                .padding(.leading)
+                .padding(.top, 40)
+                .font(.largeTitle)
+            
+            GeometryReader { geo in
+                
+                TabView {
+                    
+                    // Loop through each recipe
+                    ForEach (0..<model.recipes.count) { index in
+                        
+                        // Only show those recipes with featured equals true
+                        if model.recipes[index].featured == true {
+                            
+                            // Recipe Card
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                        
+                                VStack(spacing: 0){
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                        .clipped()
+                                    Text(model.recipes[index].name)
+                                        .padding(5)
+                                }
+                             
+                            }
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .cornerRadius(15)
+                            .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: 5)
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            
+            VStack (alignment: .leading, spacing: 10) {
+                Text("Preparation Time:")
+                    .font(.headline)
+                Text("1 hour")
+                Text("Highlight:")
+                    .font(.headline)
+                Text("Healthy")
+            }
+            .padding(.leading)
+            .padding(.bottom)
+        }
+        
+        
     }
 }
 
